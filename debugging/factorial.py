932fff99847a536138614e7e64375003c8afcada
“xtvkyx <ljabli@icloud.com>
2025-11-23 13:06:49 +0000

    0. Debugging - Python Factorial

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -2,11 +2,11 @@
 import sys
 
 def factorial(n):
-	result = 1
-	while n > 1:
-	result *= n
-	n -= 1
-return result
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1
+    return result
 
 f = factorial(int(sys.argv[1]))
 print(f)
